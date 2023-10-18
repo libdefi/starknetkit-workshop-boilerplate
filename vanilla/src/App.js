634de745b8bc8e62895c2aeb4d0c0f403e0b6b1b
libdefi <libcreatesweb3@gmail.com>
2023-10-18 21:42:35 +0800

    Connecting and disconnecting, getting counts

--- vanilla/src/App.js
+++ vanilla/src/App.js
@@ -1,7 +1,7 @@
 import './App.css'
 import { connect, disconnect } from 'starknetkit'
 import { useState, useEffect } from 'react'
-import { Contract } from 'starknet'
+import { Contract, Provider, constants } from 'starknet'
 
 import contractAbi from './abis/abi.json'
 const contractAddress = "0x077e0925380d1529772ee99caefa8cd7a7017a823ec3db7c003e56ad2e85e300"
@@ -14,10 +14,34 @@ function App() {
   const [retrievedValue, setRetrievedValue] = useState('')
 
   const connectWallet = async() => {
+    const connection = await connect({webWalletUrl: "https://web.argent.xyz/"});
 
+    if (connection && connection.isConnected) {
+      setConnection(connection);
+      setAccount(connection.account);
+      setAddress(connection.selectedAddress);
+    }
+    if(connection && connection.id !== "argentWebWallet" && connection.chainId !== "SN_MAIN") {
+      try{
+        await window.starknet.request({
+          type: "wallet_switchStarknetChain",
+          params: {
+            chainId: "SN_MAIN"
+          }
+        });
+      }
+      catch(error) {
+        // console.log(error)
+        alert("please switch to Mainnet")
+      }
+    }
   }
 
   const disconnectWallet = async() => {
+    await disconnect();
+    setConnection(undefined);
+      setAccount(undefined);
+      setAddress('');
 
   }
 
@@ -30,6 +54,19 @@ function App() {
   }
 
   const getCounter = async() => {
+    const provider = new Provider({
+      sequencer: {
+        network: constants.NetworkName.SN_MAIN,
+      }
+    });
+    try {
+      const contract = new Contract(contractAbi, contractAddress, provider);
+      const counter = await contract.get_current_count();
+      setRetrievedValue(counter.toString());
+    }
+    catch(error) {
+      console.log(error.message)
+    }
 
   }
 
@@ -40,10 +77,16 @@ function App() {
           <h1 className="title">
             Starknet<a href="#"> Counter</a>
           </h1>
-            <button className="connect" onClick={connectWallet}>Connect wallet</button>
+          {
+            connection ? 
+              <button className="connect" onClick={disconnectWallet}>Disconnect wallet</button>
+            : 
+              <button className="connect" onClick={connectWallet}>Connect wallet</button>
+          }
+            
 
           <p className="description">
-          {/* address */}
+          { address }
           </p>
 
           <div className="grid">
